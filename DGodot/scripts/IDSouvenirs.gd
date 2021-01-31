cba64eaaebae6bc6505a3688f6289a228362187c
Paulette005 <paulette@zaclys.net>
2021-01-31 13:56:01 +0100

    maj

---
+++ DGodot/scripts/IDSouvenirs.gd
@@ -0,0 +1,6 @@
+extends Sprite
+
+export var ID = 1
+
+func _ready():
+	pass
--- DGodot/scripts/Souvenirs.gd
+++ DGodot/scripts/Souvenirs.gd
@@ -1,10 +1,11 @@
-extends Sprite
+extends Control
 
-var array = ["premier souvenir avec la route.",
+var DialSouvenir = ["premier souvenir avec la route.",
 			"deuxième souvenir ave les barricades",
 			"troisième souvenir avec les ruines champignons.",
 			"quatrième souvenir",
 			"cinquième, etc, etc.."]
+var ID
 
 func _process(delta):
-		print(array[1])
+		$RichTextLabel.bbcode_text= DialSouvenir[1]
