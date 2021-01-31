b9cd1bb14e906bc1c2cf6867ae4a8062c7e51d50
Paulette005 <paulette@zaclys.net>
2021-01-31 12:10:39 +0100

    Ménage du code

--- DGodot/scripts/Souvenirs.gd
+++ DGodot/scripts/Souvenirs.gd
@@ -1,4 +1,10 @@
 extends Sprite
 
-export var NomSouvenir = ""
-var Dialogues = 0
+var array = ["premier souvenir avec la route.",
+			"deuxième souvenir ave les barricades",
+			"troisième souvenir avec les ruines champignons.",
+			"quatrième souvenir",
+			"cinquième, etc, etc.."]
+
+func _process(delta):
+		print(array[1])
--- DGodot/scripts/Zindex.gd
+++
@@ -1,5 +0,0 @@
-extends Sprite
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	z_index = global_position.y
