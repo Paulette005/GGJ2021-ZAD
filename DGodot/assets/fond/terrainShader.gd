185ecff21022dd678dad874f307d8ee37463e21b
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-02-02 10:02:35 +0100

    shader terrain

---
+++ DGodot/assets/fond/terrainShader.gd
@@ -0,0 +1,11 @@
+tool
+extends Sprite
+
+
+func calculate_size ():
+	material.set_shader_param("rect_size", texture.get_size()*scale)
+	pass
+	
+func _ready():
+	material.set_shader_param("rect_size", texture.get_size()*scale)
+	#print (texture.get_size()*scale)
