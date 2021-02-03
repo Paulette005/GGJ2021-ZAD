84ee22ffb42f2c77b92eb211b67d04e7aa5bf8e8
Paulette005 <paulette@zaclys.net>
2021-02-03 11:42:35 +0100

    narration

--- DGodot/scripts/IDSouvenirs.gd
+++ DGodot/scripts/IDSouvenirs.gd
@@ -1,6 +1,4 @@
 extends Node2D
 
 export var IDSouvenirs = 0
-
-
-
+var Narration = 0
--- DGodot/scripts/Interactions.gd
+++ DGodot/scripts/Interactions.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var playerZone = false
 var IDSouvenirs
+var Narration = 0
 
 onready var Dialogues = get_node("/root/Node2D/CanvasLayer/Dialogues")
 onready var BoiteDialogues = get_node("/root/Node2D/CanvasLayer/Dialogues/BoiteDialogues")
@@ -13,19 +14,27 @@ func _ready():
 func on_body_entered(body):
 	if body.name == "Player":
 		playerZone = true
-		print("playerZone true")
+		BoiteDialogues.Souvenirs = BoiteDialogues.DialSouvenir[IDSouvenirs]
+		if IDSouvenirs == 0:
+			parle()
 
 func on_body_exited(body):
 	if body.name == "Player":
 		playerZone = false
 		Dialogues.visible = false
-		print("playerZone false")
 
 func _input(event):
 	if event.is_action_pressed("ui_accept") && playerZone:
 		parle()
 
 func parle():
-	BoiteDialogues.Souvenirs = BoiteDialogues.DialSouvenir[IDSouvenirs]
+	if IDSouvenirs == 0:
+		if Narration == 0:
+			BoiteDialogues.Souvenirs = BoiteDialogues.DialSouvenir[Narration]
+		elif Narration == 6:
+			BoiteDialogues.Souvenirs = BoiteDialogues.DialSouvenir[Narration]
+		elif Narration == 5:
+			BoiteDialogues.Souvenirs = BoiteDialogues.DialSouvenir[Narration]
+	else:
+		BoiteDialogues.Souvenirs = BoiteDialogues.DialSouvenir[IDSouvenirs]
 	Dialogues.visible = true
-	print(BoiteDialogues.DialSouvenir)
