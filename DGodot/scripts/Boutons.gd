b2d932d1824f88ec315aac7d9c88123127609328
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-02-09 15:57:35 +0100

    cacher curseur

--- DGodot/scripts/Boutons.gd
+++ DGodot/scripts/Boutons.gd
@@ -2,6 +2,7 @@ extends TextureButton
 
 func _ready():
 	get_node("/root/Control/VBoxContainer/Jouer").grab_focus()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _on_Jouer_pressed():
 	get_tree().change_scene("res://DGodot/scenes/scene_LD2.tscn")
